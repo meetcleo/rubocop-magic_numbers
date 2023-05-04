7667606f447973b017fe4beb33dcb906051a3fc8
Gavin Morrice <gavin@gavinmorrice.com>
2023-05-04 17:43:36 +0100

    Support custom config in base class

--- lib/rubocop/cop/magic_numbers/base.rb
+++ lib/rubocop/cop/magic_numbers/base.rb
@@ -5,11 +5,55 @@ module RuboCop
     module MagicNumbers
       # Base class for all shared behaviour between these cops
       class Base < ::RuboCop::Cop::Cop
-        ILLEGAL_SCALAR_TYPES = %i[float int].freeze
-        ILLEGAL_SCALAR_PATTERN = "{#{ILLEGAL_SCALAR_TYPES.join(' ')}}".freeze
+        CONFIG_ALL = 'All'
+        CONFIG_FLOAT = 'Float'
+        CONFIG_INTEGER = 'Integer'
+        CONFIG_NAME_FORBIDDEN_NUMERICS = 'ForbiddenNumerics'
+
+        DEFAULT_CONFIG = {
+          CONFIG_NAME_FORBIDDEN_NUMERICS => CONFIG_ALL
+        }.freeze
+
+        ILLEGAL_SCALAR_TYPES = {
+          CONFIG_ALL => %i[float int],
+          CONFIG_INTEGER => %i[int],
+          CONFIG_FLOAT => %i[float]
+        }.freeze
+
+        # The configuration for this cop, pre-set with defaults
+        #
+        # Returns Hash
+        def cop_config
+          DEFAULT_CONFIG.merge(super)
+        end
 
         private
 
+        # The AST pattern for the configured ForbiddenNumerics types.
+        #
+        # Examples
+        #
+        #   "{float int}"
+        #
+        # Returns String
+        def illegal_scalar_pattern
+          "{#{forbidden_numerics.join(' ')}}"
+        end
+
+        # The numeric types that are forbidden based on this cop's configuration
+        #
+        # Returns Array of Symbols
+        def forbidden_numerics
+          forbidden_numerics_key = cop_config[CONFIG_NAME_FORBIDDEN_NUMERICS]
+          ILLEGAL_SCALAR_TYPES[forbidden_numerics_key]
+        end
+
+        # Check if the given AST node matches the given pattern
+        #
+        # node    - A RuboCop::AST::ProcessedSource
+        # pattern - A RuboCop::AST::NodePattern
+        #
+        # Returns Boolean
         def node_matches_pattern?(node:, pattern:)
           RuboCop::AST::NodePattern.new(pattern).match(node)
         end
