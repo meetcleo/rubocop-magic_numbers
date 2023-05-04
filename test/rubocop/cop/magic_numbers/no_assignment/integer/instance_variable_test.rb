7b3bf1bcfb6c1b7b69171e043dd7423987e676ae
Fell Sunderland <fell@meetcleo.com>
2023-05-04 11:22:18 +0100

    Add testing for NoAssignment cop config for specific numeric types

---
+++ test/rubocop/cop/magic_numbers/no_assignment/integer/instance_variable_test.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'test_helper'
+require 'rubocop/no_magic_numbers'
+
+module RuboCop
+  module Cop
+    module MagicNumbers
+      class NoAssignment
+        module Integer
+          class InstanceVariableTest < Minitest::Test
+            def setup
+              @matched_numerics = TestHelper::INTEGER_LITERALS
+            end
+
+            def test_detects_magic_numbers_assigned_to_instance_variables
+              @matched_numerics.each do |num|
+                inspect_source(<<~RUBY)
+                  def test_method
+                    @instance_variable = #{num}
+                  end
+                RUBY
+
+                assert_instance_variable_offense
+
+                reset_cop
+              end
+            end
+
+            private
+
+            def assert_instance_variable_offense
+              assert_offense(
+                cop_name: cop.name,
+                violation_message: described_class::INSTANCE_VARIABLE_ASSIGN_MSG
+              )
+            end
+
+            def described_class
+              RuboCop::Cop::MagicNumbers::NoAssignment
+            end
+
+            def cop
+              @cop ||= described_class.new(config)
+            end
+
+            def reset_cop
+              @cop = nil
+            end
+
+            def config
+              @config ||= RuboCop::Config.new(
+                'MagicNumbers/NoAssignment' => {
+                  'Enabled' => true,
+                  'ForbiddenNumerics' => 'Integer'
+                }
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
