a3153713fe5ce1d032849fcd0b548b051f613070
Gavin Morrice <gavin@gavinmorrice.com>
2023-05-04 16:41:11 +0100

    Fix rubocop violations

--- lib/rubocop/cop/magic_numbers/base.rb
+++ lib/rubocop/cop/magic_numbers/base.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 module RuboCop
   module Cop
     module MagicNumbers
+      # Base class for all shared behaviour between these cops
       class Base < ::RuboCop::Cop::Cop
         ILLEGAL_SCALAR_TYPES = %i[float int].freeze
-        ILLEGAL_SCALAR_PATTERN = "{#{ILLEGAL_SCALAR_TYPES.join(" ")}".freeze
+        ILLEGAL_SCALAR_PATTERN = "{#{ILLEGAL_SCALAR_TYPES.join(' ')}".freeze
 
         private
 
