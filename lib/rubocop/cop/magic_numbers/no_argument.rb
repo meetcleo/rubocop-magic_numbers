b5420ed7f252b4a63086782849bbd59c215409b4
Bodacious <gavin@gavinmorrice.com>
2023-05-07 13:48:47 +0100

    Allow magic numbers as collection index arguments

--- lib/rubocop/cop/magic_numbers/no_argument.rb
+++ lib/rubocop/cop/magic_numbers/no_argument.rb
@@ -44,6 +44,18 @@ module RuboCop
 
         ARGUMENT_MSG = 'Do not use magic number arguments to methods'
 
+        CONFIG_IGNORED_METHODS_NAME = "IgnoredMethods"
+
+        # By default, don't raise an offense for magic numbers arguments
+        # for these methods
+        DEFAULT_CONFIG = {
+          CONFIG_IGNORED_METHODS_NAME => ['[]']
+        }
+
+        def cop_config
+          super.merge(DEFAULT_CONFIG)
+        end
+
         def on_method_defined(node)
           return unless illegal_positional_default?(node)
 
@@ -57,12 +69,23 @@ module RuboCop
 
         def on_message_send(node)
           return unless illegal_argument?(node)
+          return if ignored_method?(node)
 
           add_offense(node, location: :expression, message: ARGUMENT_MSG)
         end
         alias on_send on_message_send # rubocop API method name
 
         private
+
+        def ignored_method?(node)
+          return false unless node.respond_to?(:method_name)
+
+          ignored_methods.include?(node.method_name)
+        end
+
+        def ignored_methods
+          Array(cop_config[CONFIG_IGNORED_METHODS_NAME]).map(&:to_sym)
+        end
 
         def illegal_positional_default?(node)
           node_matches_pattern?(
